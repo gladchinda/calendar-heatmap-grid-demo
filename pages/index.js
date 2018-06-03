83eceed91532ada1deded720852a259276eba9d8
Glad Chinda <gladxeqs@gmail.com>
2018-06-03 18:56:25 +0100

    Added the index page component

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+	return <div className="container-fluid position-absolute h-100">
+		<div className="d-flex position-relative h-100 w-100 flex-wrap justify-content-center align-items-center align-content-center">
+			<h1 className="font-weight-light">Calendar Heatmap</h1>
+		</div>
+	</div>
+};
