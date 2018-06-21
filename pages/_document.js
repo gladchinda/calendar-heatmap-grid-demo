f28af7251ad437e64cf88be4ba78ca9df1786de4
Glad Chinda <gladxeqs@gmail.com>
2018-06-21 09:37:06 +0100

    Modified _document to include compiled css file

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 
 class AppDocument extends Document {
@@ -9,10 +10,13 @@ class AppDocument extends Document {
 					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 					<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
+					<title>Calendar Heatmap</title>
+
 					{/* <!-- Bootstrap CSS --> */}
 					<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
 
-					<title>Calendar Heatmap</title>
+					{/* Compiled CSS file for our app */}
+					<link rel="stylesheet" href="/_next/static/style.css" />
 				</Head>
 				<body>
 					<Main />
